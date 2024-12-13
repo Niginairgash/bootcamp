cbef8e2c8f4a0550f8ccccd38fc1e2e3b8f099fb
Niginairgash <40998910+Niginairgash@users.noreply.github.com>
2024-12-13 17:00:43 +0500

    Update homework.sql

--- 1-dimensional-data-modeling/homework.sql
+++ 1-dimensional-data-modeling/homework.sql
@@ -1,13 +1,30 @@
 
- /*  create type films as(
+ /*
+An array of struct with the following fields:
+	*film: The name of the film.
+	*votes: The number of votes the film received.
+	*rating: The rating of the film.
+	*filmid: A unique identifier for each film.
+*/
+create type films as(
  	films text,
  	votes int4,
  	rating float4,
  	filmid text
  )
- 
+
+/*
+quality_class: This field represents an actor's performance quality, determined by the average rating of movies of their most recent year. It's categorized as follows:
+	*star: Average rating > 8.
+	*good: Average rating > 7 and ≤ 8.
+	*average: Average rating > 6 and ≤ 7.
+	*bad: Average rating ≤ 6.
+*/
  create type quality_class as enum('bad', 'average', 'good', 'star')
- 
+
+/*
+DDL for actors table
+*/
 create table actors(
 	actor text,
 	actor_id text,
